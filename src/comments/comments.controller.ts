b44b4fdc4d75023bd822859b74e8ebf8cdb3b886
choi ha young <gkdudk15@naver.com>
2023-08-23 17:37:32 +0900

    feat: comment Get요청 성공
    DB실행해서 Get요청에 성공하였습니다.

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto/create.comments.dto';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+    constructor(private readonly commentsService: CommentsService) {}
+
+
+    @Get()
+    async getAllComments() {
+        return this.commentsService.getAllComments()
+    }
+
+    @Post(":id")
+    async createComment(
+        @Param('id') id: string,
+        @Body() body: CreateCommentDto,
+    ) {
+        return this.commentsService.createComment(id, body)
+    }
+}
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Get, Injectable, Param, Post } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCommentDto } from './dto/create.comments.dto';
+import { User } from 'src/entity/user.entity';
+import { Feed } from 'src/entity/feed.entity';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+    // constructor(
+    //     @InjectRepository(User)
+    //      private userRepository: Repository<User>,
+    //      @InjectRepository(Feed)
+    //      private feedRepository: Repository<Feed>
+    //         ) {}
+            
+    @Get()
+    async getAllComments() {
+        return 'hello world'
+    }
+    @Post(':id')
+    async createComment( id: string, comments: CreateCommentDto){
+        return 'hello world'
+    }
+
+}
+

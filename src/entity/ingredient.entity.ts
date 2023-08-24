20362ec8c4c3974301bc7c03e08f0fe8b1456cec
Lee SeoWon <rymil9303@gmail.com>
2023-08-24 16:57:15 +0900

    fix: 엔티티때문에 서버 구동 안되던거 수정(수정자:이서원)
    ingredient.entity.ts에 @Unique(['name'])과 unique:true가 중복되어있어서
    서버가 올라가지 않았던 문제
    ingredient 엔티티에서 @Unique를 제거하여 서버 구동 에러를 수정했다.

--- src/entity/ingredient.entity.ts
+++ src/entity/ingredient.entity.ts
@@ -1,16 +1,10 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  Unique,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity({ schema: 'finalpj', name: 'ingredient' })
-@Unique(['name'])
 export class Ingredient {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column({ type: 'varchar', length: 255, unique: true })
-  name: string;  
+  name: string;
 }

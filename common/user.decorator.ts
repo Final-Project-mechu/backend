97fbef7906b02d184e0ecc3d9336544b8d6e625a
choi ha young <gkdudk15@naver.com>
2023-08-29 20:26:23 +0900

    Merge branch 'develop' of https://github.com/Final-Project-mechu/backend into feature-comment
    Co-authored-by: Sangwoo rhie <sangwoorhie@users.noreply.github.com>

--- common/user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { User } from 'src/entity/user.entity';
-
-export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): User => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
-  },
-);
--- src/food/food.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FoodService } from './food.service';
-import { FoodController } from './food.controller';
-
-@Module({
-  providers: [FoodService],
-  controllers: [FoodController]
-})
-export class FoodModule {}

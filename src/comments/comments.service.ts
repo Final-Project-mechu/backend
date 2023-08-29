8bd66a1fda5527c45e0e1d2c87d1582b322c087e
choi ha young <gkdudk15@naver.com>
2023-08-29 09:57:59 +0900

    feat: 해당 댓글 권한 부여가 되지 않음 해당 유저만 수정 삭제 할수 있게 구현되어야함

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -21,29 +21,15 @@ export class CommentsService {
     private readonly feedRepository: Repository<Feed>,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-
-    
   ) {}
 
   // 게시글 ID로 댓글 전체 조회
   async getCommentsByFeedId(feed_id: number) {
-    // const feedId = await this.feedRepository.findOne({})
-    // if (!feedId) {
-    //   throw new NotFoundException(`게시글이 조회되지 않습니다.`);
-    // }
-  console.log('Service',feed_id);
-const result = await this.commentRepository.query(`select * from comment where feed_id=${feed_id} and deletedAt is null`)
-    // const result = await this.commentRepository.find({
-    //   where: [{ deletedAt: null }, { id: feed_id}],
-    //   select: ['contents', 'nick_name', 'createdAt', 'updatedAt'],
-    // });
-    // console.log(result);
-    
-    return result
-    // if (!comment) {
-    //   throw new NotFoundException(`댓글이 존재하지 않습니다.`);
-    // }
-    // return comment;
+    const result = await this.commentRepository.query(
+      `select * from comment where feed_id=${feed_id} and deletedAt is null`,
+    );
+
+    return result;
   }
 
   // 댓글 생성
@@ -51,15 +37,13 @@ const result = await this.commentRepository.query(`select * from comment where f
     user_id: number,
     feedId: number,
     createCommentDto: CreateCommentDto,
-  ) 
-  {
+  ) {
     const user = await this.userRepository.findOne({
       where: { id: user_id },
     });
     if (!user) {
       throw new UnauthorizedException(`로그인 후 댓글 작성이 가능합니다.`);
     }
-    const nickname = user.nick_name
 
     const feed = await this.feedRepository.findOne({
       where: { id: feedId },
@@ -74,7 +58,7 @@ const result = await this.commentRepository.query(`select * from comment where f
     const feedComment = this.commentRepository.create({
       user_id,
       feedId,
-      contents
+      contents,
     });
     return await this.commentRepository.save(feedComment);
   }
@@ -85,10 +69,9 @@ const result = await this.commentRepository.query(`select * from comment where f
     commentId: number,
     updateCommentDto: UpdateCommentDto,
   ) {
-
     const myComment = await this.commentRepository.findOne({
-      where: {id: commentId}
-    })
+      where: { id: commentId },
+    });
 
     if (!myComment) {
       throw new NotFoundException(`댓글이 조회되지 않습니다.}`);
@@ -101,13 +84,7 @@ const result = await this.commentRepository.query(`select * from comment where f
       throw new UnauthorizedException(`본인이 작성한 댓글만 수정가능합니다.`);
     }
 
-    // const feed = await this.feedRepository.findOne({
-    //   where: { id: feedId },
-    // });
-    // if (!feed) {
-    //   throw new NotFoundException(`게시글이 조회되지 않습니다.`);
-    // }
- 
+
     const { contents } = updateCommentDto;
     if (!updateCommentDto.contents) {
       throw new BadRequestException(`댓글을 입력해주세요`);
@@ -119,32 +96,25 @@ const result = await this.commentRepository.query(`select * from comment where f
   }
 
   // // 댓글 삭제
-  async deleteComment(
-    user_id: number,
-    id: number,
-  ): Promise<any> {
+  async deleteComment(user_id: number, id: number): Promise<any> {
     const user = await this.userRepository.findOne({
       where: { id: user_id },
     });
     if (!user) {
-      throw new UnauthorizedException(`본인이 작성한 댓글만 삭제가 가능합니다.`);
+      throw new UnauthorizedException(
+        `본인이 작성한 댓글만 삭제가 가능합니다.`,
+      );
     }
 
-
-
     const myComment = await this.commentRepository.findOne({
-      where: {id}
-    })
+      where: { id },
+    });
     if (!myComment) {
-      throw new NotFoundException(
-        `댓글이 조회되지 않습니다.`,
-      );
+      throw new NotFoundException(`댓글이 조회되지 않습니다.`);
     }
     const remove = await this.commentRepository.softDelete(id);
     if (remove.affected === 0) {
-      throw new NotFoundException(
-        `해당 댓글이 조회되지 않습니다.`,
-      );
+      throw new NotFoundException(`해당 댓글이 조회되지 않습니다.`);
     }
   }
 }

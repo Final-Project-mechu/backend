90a7b98da261edf04b43c1eef752b4a4fb76d7c4
seowonLee <46676916+rymile@users.noreply.github.com>
2023-09-10 17:10:55 +0900

    Update start.sh
    베포테스트14

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,14 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-APP_NAME="project"
+cd /home/ubuntu/app
 
-TIME_NOW=$(date +%c)
-
-cd $PROJECT_ROOT
-
-sudo pm2 delete $APP_NAME
-sudo pm2 "npm i"
-sudo pm2 "npm run start"
-
-echo "$TIME_NOW > Deploy has been completed"
+sudo npm install
+sudo pm2 kill
+sudo pm2 start "npm run start:prod

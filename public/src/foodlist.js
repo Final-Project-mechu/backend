de8b407ad17876d4cdf2fb589b6a2b86a6848540
HyungJin0114 <spunkup20@naver.com>
2023-09-11 17:39:00 +0900

    fix foodlist

--- public/src/foodlist.js
+++ public/src/foodlist.js
@@ -1,6 +1,25 @@
 // 카테고리 조회 함수
-// id, category_name, 
 
+
+
+// id, category_name, 
+async function categoryGet(){
+    const callCategoryServer = await axios({
+        method: 'get',
+        url:'http://localhost:3000/category',
+    });
+    const allCategory = callCategoryServer.data;
+    console.log(allCategory);
+    createAllCategoryItems(allCategory)
+}
+function createAllCategoryItems(categorys){
+    const categorysContainer = document.getElementById('category-container');
+    categorysContainer.innerHTML = '';
+    categorys.forEach(category => {
+        categorysContainer.innerHTML += `<li class="active" data-filter=".food${category.id}">${category.category_name}</li>`
+    });
+}
+categoryGet();
 
 // 음식 조회 함수
 // id, food_id, category_id, food_img, deleteAt 불러옴
@@ -18,7 +37,7 @@ function createAllFoodItems(foods){
     foodsContainer.innerHTML = '';
 
     foods.forEach(food => {
-        foodsContainer.innerHTML += `<div class="col-lg-3 col-md-6 col-sm-6 mix ${food.categort_id}">
+        foodsContainer.innerHTML += `<div class="col-lg-3 col-md-6 col-sm-6 mix food${food.category_id}">
         <div class="featured__item">
             <div class="featured__item__pic set-bg" data-setbg="${food.food_img}">
             </div>

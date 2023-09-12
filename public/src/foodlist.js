6b84e410aa2502d45354f89cb8dfa77fb98df6a2
HyungJin0114 <spunkup20@naver.com>
2023-09-12 11:08:56 +0900

    fix 음식 리스트 및 재료 불러오기 완료

--- public/src/foodlist.js
+++ public/src/foodlist.js
@@ -1,5 +1,4 @@
 // 카테고리 조회 함수
-
 // id, category_name,
 async function categoryGet() {
   const callCategoryServer = await axios({
@@ -40,7 +39,7 @@ function createAllFoodItems(foods) {
             <div class="featured__item__pic set-bg" data-setbg="${food.food_img}">
             </div>
             <div class="featured__item__text">
-                <h6><button id="openNewWindowBtn"><a href="#">${food.food_name}</a></button></h6>
+                <h6><button onclick='foodInfo(${food.id})' >${food.food_name}</button></h6>
             </div>
         </div>
     </div>`;
@@ -48,30 +47,74 @@ function createAllFoodItems(foods) {
 }
 foodGet();
 
-document
-  .getElementById('openNewWindowBtn')
-  .addEventListener('click', function () {
-    const foodsIngredientData = createAllFoodIngredient;
+// window.onload = function () {
+//   document
+//     .getElementById('openNewWindowBtn')
+//     .addEventListener('click', async function () {
+//       const fId = document.getElementById('openNewWindowBtn');
+//       const foodId = fId.getAttribute('data-food-id');
+//       console.log(foodId);
 
-    const foodIgredientHtml = foodsIngredientData
-      .map(item => `<li>${ingredient.name}</li>`)
-      .join('');
+//       url = `http://localhost:3000/food/${Number(foodId)}`;
+//       const callFoodIngredient = await axios.get(url);
+//       const allFoodsIngredient = callFoodIngredient.data;
 
-    const newWindow = window.open('', 'userDataWindow', 'width=400,height=300');
-    newWindow.document.body.innerHTML = `
-         <ul>
-            ${foodIgredientHtml}
-         </ul>
-        `;
-  });
+//       const foodIgredientHtml = allFoodsIngredient
+//         .map(item => `<li>${item.ingredient_name}</li>`)
+//         .join('');
 
-async function foodIngredient() {
-  const callFoodIngredient = await axios({
-    method: 'get',
-    url: 'http://localhost:3000/food/:food_id',
-  });
+//       console.log(foodIgredientHtml);
+//       const newWindow = window.open(
+//         '',
+//         'userDataWindow',
+//         'width=400,height=300',
+//       );
+//       newWindow.document.body.innerHTML = `
+//          <ul>
+//             <h2><h2>
+//             <h3>재료</h3>
+//             ${foodIgredientHtml}
+//          </ul>
+//         `;
+//     });
+// };
+
+async function foodInfo(foodId) {
+  console.log(foodId);
+  url = `http://localhost:3000/food/${Number(foodId)}`;
+  const callFoodIngredient = await axios.get(url);
   const allFoodsIngredient = callFoodIngredient.data;
-  createAllFoodIngredient(allFoodsIngredient);
+
+  const foodIgredientHtml = allFoodsIngredient
+    .map(item => `<li>${item.ingredient_name}</li>`)
+    .join('');
+  console.log(foodIgredientHtml);
+
+  var popupW = 400;
+  var popupH = 300;
+  var left = Math.ceil((window.screen.width - popupW) / 2);
+  var top = Math.ceil((window.screen.height - popupH) / 2);
+
+  const newWindow = window.open(
+    '',
+    'userDataWindow',
+    'width=' +
+      popupH +
+      ',height=' +
+      popupH +
+      ',left=' +
+      left +
+      ',top=' +
+      top +
+      ',scrollbars=yes,resizable=no,toolbar=no,titlebar=no,menubar=no,location=no',
+  );
+  newWindow.document.body.innerHTML = `
+   <ul>
+      <h2><h2>
+      <h3>재료</h3>
+      ${foodIgredientHtml}
+   </ul>
+  `;
 }
 
 // <h6><a href="http://localhost:3000/food/${food.id}">${food.food_name}</a></h6>
